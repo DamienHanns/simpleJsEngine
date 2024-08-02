6df7bfc2c42311b9f91da03508158bd90081ebbf
Damien Hanns <damienhanns@Damiens-MacBook-Pro.local>
2024-08-02 14:19:15 +0100

    bacic collisions  added

--- ECS/Systems/CalculateCollisionSystem.js
+++ ECS/Systems/CalculateCollisionSystem.js
@@ -16,35 +16,14 @@ export class CalculateCollisionSystem extends System {
 
         if (this.systemEntities === undefined) { console.log('has no entities to run on'); return; }
 
+        //todo, take movment into account. At current all others need a movementComponent in order to register collisions.
         for (let i = 0; i < this.systemEntities.length; i++) {
-
             const moveComponent = this.ecs.getComponent(this.systemEntities[i], MovementComponent);
             const collisionComponent = this.ecs.getComponent(this.systemEntities[i], CollisionRectComponent);
             const positionComponent = this.ecs.getComponent(this.systemEntities[i], PositionComponent);
 
-            const xVelocity = moveComponent.xVelocity;
-            const yVelocity = moveComponent.yVelocity;
-
-            //todo adjust for offsets.
-            const xTopLeft = positionComponent.x;
-            const yTopLeft = positionComponent.y;
-
-            const xTopRight = positionComponent.x + collisionComponent.width;
-            const yTopRight = positionComponent.y;
-
-            const xBottomLeft = positionComponent.x;
-            const yBottomLeft = positionComponent.y + collisionComponent.height;
-
-            const xBottomRight = positionComponent.x + collisionComponent.width;
-            const yBottomRight = positionComponent.y + collisionComponent.height;
-
-            //console.log('xTopLeft: ', Math.floor(xTopLeft * 100) / 100 , 'yTopLeft',Math.floor(yTopLeft * 100) / 100);
-           // console.log('xTopRight: ', Math.floor(xTopRight * 100) / 100 , 'yTopRight',Math.floor(yTopRight * 100) / 100);
-
-           // console.log('xBottomLeft: ', Math.floor(xBottomLeft * 100) / 100 , 'yBottomLeft',Math.floor(yBottomLeft * 100) / 100);
-           // console.log('xBottomRight: ', Math.floor(xBottomRight * 100) / 100 , 'yBottomRight',Math.floor(yBottomRight * 100) / 100);
-
             for (let j = 0; j < this.systemEntities.length; j++){
+                //avoid self collisions
                 if (j === i) { continue;}
 
                 const otherMoveComponent = this.ecs.getComponent(this.systemEntities[j], MovementComponent);
